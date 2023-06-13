74f1b7ec8f72177fd60279b551eb721b3fa8d4ff
Josh <zylo.codes@gmail.com>
2023-06-12 20:34:49 -0500

    pseudocode

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
-console.log("Hello RPS!")
+// variables
+  // num wins = 0
+  // num losses 
+  // num ties
+
+// players choose R, P, S
+  // human choice - get choice from user input
+  // computer randomly chooses
+
+// compare choices
+  // if humanChoice is S and computerChoice is P OR
+  // if humanChoice is R and computerChoice is S OR
+  // if humanChoice is P and computerChoice is R
+    // alert that we won
+    // increase wins
+  // if humanChoice === computerChoice
+    // alert that we tied
+    // increase ties 
+  // else
+    // alert that lost
+    // increase losses
+
+// ask user "do you want to play again?"
+  // if yes
+    // restart game
+  // if no
+    // exit game
